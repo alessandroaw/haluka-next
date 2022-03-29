efe3ec9e451a014abc7669f16d2ee9cd4369a01c
sandro <assandroa95@gmail.com>
2022-03-29 22:01:17 +0700

    Refactor package json and fix scrollbar overflow

--- src/modules/callHistory/callHistoryPage.tsx
+++ src/modules/callHistory/callHistoryPage.tsx
@@ -1,9 +1,9 @@
+import { NextPage } from "next";
+import React from "react";
+import { Box, Container, Typography } from "@mui/material";
+import { MainAppBar } from "src/components/appBar";
 import { CallHistoryDataGrid } from "./callHistoryDataGrid";
 import { CallHistoryFilter } from "./callHistoryFilter";
-import { Container } from "@mui/material";
-import { NextPage } from "next";
-import { MainAppBar } from "src/components/appBar";
-import { Typography, Box } from "@mui/material";
 
 export const CallHistoryPage: NextPage = () => {
   return (
