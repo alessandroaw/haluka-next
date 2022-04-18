50640280dffab94d4b4d59e9534e8d4a861b90db
sandro <assandroa95@gmail.com>
2022-04-18 10:12:18 +0700

    refactor allocation mutation

--- src/modules/settings/wartelSettingsList/wartelNameAndAllocation.tsx
+++ src/modules/settings/wartelSettingsList/wartelNameAndAllocation.tsx
@@ -2,7 +2,7 @@ import { CircularProgress, Stack, Typography } from "@mui/material";
 import React from "react";
 import { Button } from "src/components/button";
 import { allocateBooth, deallocateBooth } from "src/repositories/booths";
-import { clientKey } from "src/swr-cache/useClient";
+import { clientKey, useClient } from "src/swr-cache/useClient";
 import { userBoothsByIdKeys } from "src/swr-cache/useUserBoothsById";
 import { userListKey } from "src/swr-cache/useUserList";
 import { Booth, Client, User } from "src/types/models";
@@ -15,27 +15,70 @@ interface WartelNameAndAllocationProps {
 export const WartelNameAndAllocation: React.FC<
   WartelNameAndAllocationProps
 > = ({ wartel }) => {
+  const { client, mutate: mutateClient } = useClient();
   const [loading, setLoading] = React.useState(false);
+
+  const fullMutation = (
+    allocate: boolean,
+    createdBooth: Booth | undefined = undefined
+  ) => {
+    if (allocate && !createdBooth)
+      throw new Error("createdBooth cannot undefined in allocation");
+
+    clientMutation(allocate);
+    userBoothMutation(allocate, createdBooth);
+    userListMutation(allocate);
+  };
+
+  const clientMutation = (allocate: boolean) => {
+    const increment = allocate ? 1 : -1;
+    if (client) {
+      const newClient = { ...client };
+      newClient.currentBoothCount += increment;
+      mutateClient(newClient);
+    } else {
+      mutate((client: Client) => {
+        client.currentBoothCount += increment;
+        return client;
+      });
+    }
+  };
+
+  const userBoothMutation = (
+    allocate: boolean,
+    createdBooth: Booth | undefined = undefined
+  ) => {
+    if (allocate && createdBooth) {
+      mutate(userBoothsByIdKeys(wartel.id), (boothList: Booth[]) => [
+        ...boothList,
+        createdBooth,
+      ]);
+    } else {
+      mutate(userBoothsByIdKeys(wartel.id), (boothList: Booth[]) => {
+        const newBoothList = [...boothList];
+        newBoothList.pop();
+        return newBoothList;
+      });
+    }
+  };
+
+  const userListMutation = (allocate: boolean) => {
+    mutate(userListKey, (userList: User[]) => {
+      const userIndex = userList.findIndex((u) => u.id === wartel.id);
+      const lastNumber: number = userList[userIndex].lastBoothNumber ?? 0;
+      const newUserList = [...userList];
+      newUserList[userIndex].lastBoothNumber = allocate
+        ? lastNumber + 1
+        : lastNumber - 1;
+      return [...newUserList];
+    });
+  };
 
   const handleAllocation = async () => {
     setLoading(true);
     try {
       const createdBooth = await allocateBooth(wartel.id);
-      mutate(userBoothsByIdKeys(wartel.id), (boothList: Booth[]) => [
-        ...boothList,
-        createdBooth,
-      ]);
-      mutate(userListKey, (userList: User[]) => {
-        const userIndex = userList.findIndex((u) => u.id === wartel.id);
-        const lastNumber: number = userList[userIndex].lastBoothNumber ?? 0;
-        const newUserList = [...userList];
-        newUserList[userIndex].lastBoothNumber = lastNumber + 1;
-        return [...newUserList];
-      });
-      mutate(clientKey, (client: Client) => {
-        client.currentBoothCount += 1;
-        return client;
-      });
+      fullMutation(true, createdBooth);
     } catch (e) {
       alert("kesalahan");
     }
@@ -46,26 +89,16 @@ export const WartelNameAndAllocation: React.FC<
     setLoading(true);
     try {
       await deallocateBooth(wartel.id);
-      mutate(userBoothsByIdKeys(wartel.id), (boothList: Booth[]) => {
-        const newBoothList = [...boothList];
-        newBoothList.pop();
-        return newBoothList;
-      });
-      mutate(userListKey, (userList: User[]) => {
-        const userIndex = userList.findIndex((u) => u.id === wartel.id);
-        const lastNumber: number = userList[userIndex].lastBoothNumber ?? 0;
-        const newUserList = [...userList];
-        newUserList[userIndex].lastBoothNumber = lastNumber - 1;
-        return [...newUserList];
-      });
-      mutate(clientKey, (client: Client) => {
-        client.currentBoothCount -= 1;
-        return client;
-      });
+      fullMutation(false);
     } catch (e) {
       alert("kesalahan");
     }
     setLoading(false);
+  };
+
+  const isMaxAllocation = () => {
+    if (!client) return true;
+    return client.currentBoothCount >= client.maxBooth;
   };
 
   return (
@@ -107,7 +140,7 @@ export const WartelNameAndAllocation: React.FC<
         </Stack>
         <Button
           onClick={handleAllocation}
-          disabled={loading}
+          disabled={loading || isMaxAllocation()}
           sx={{
             borderRadius: "0 7px 7px 0",
             i: {
