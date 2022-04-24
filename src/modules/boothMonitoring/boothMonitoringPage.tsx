e298a1e5a976321b2161f2299f4eb0eb014a313e
sandro <assandroa95@gmail.com>
2022-04-24 12:58:41 +0700

    Change divider to be full screen width

--- src/modules/boothMonitoring/boothMonitoringPage.tsx
+++ src/modules/boothMonitoring/boothMonitoringPage.tsx
@@ -1,7 +1,9 @@
+import { Divider } from "@mui/material";
 import { NextPage } from "next";
 import { MainAppBar } from "src/components/appBar";
 import { HalukaContainer } from "src/components/container";
 import { PageTitle } from "src/components/typography";
+import { kBorderColor } from "src/utils/styles";
 import { BoothBillPanels } from "./boothBillsPanel";
 import { BoothMonitoringHeading } from "./boothMonitoringHeading";
 
@@ -13,6 +15,7 @@ export const BoothMonitoringPage: NextPage = () => {
         <PageTitle title="Monitoring KBU" />
         <BoothMonitoringHeading />
       </HalukaContainer>
+      <Divider sx={{ borderWidth: "1px", borderColor: kBorderColor }} />
       <BoothBillPanels />
     </>
   );
